4f90d6e549debdfe7401634b4ebb2743508dbf57
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-11-07 14:36:47 +0530

    start with post routes

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,7 +1,12 @@
 const {Router} = require("express");
 const router = Router();
-const getAllPosts = require("../controllers/postControllers")
+const {getPosts, createPost, updatePost, deletePost, getSingleUserPosts} = require("../controllers/postControllers")
+
 
 router.get('/get', getAllPosts)
+router.get('/get/:userId', getSingleNotes)
+router.post('/create', createNote)
+router.put('/update/:id', updateNote)
+router.delete('/delete/:id', deleteNote)
 
 module.exports = router;
