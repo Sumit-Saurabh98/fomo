1385e15f6dc73b4df8a2df43314adb4c07c9cae4
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2025-01-06 14:44:32 +0530

    set up prisma postgres db

--- backend/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require("express");
-const router = Router();
-const {signup, login} = require("../controllers/authControllers")
-
-router.post('/signup', signup)
-router.post('/login', login)
-
-module.exports = router;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello homepage</h1>
+    </div>
+  );
+}
